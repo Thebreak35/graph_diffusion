761d2a1a1637863ce794d83a675abca735334296
Thebreak35 <3gthebreak@gmail.com>
2018-11-14 00:02:39 +0700

    independent png

--- independent.py
+++ independent.py
@@ -11,6 +11,35 @@ def infect(prob):
 		return True
 	return False
 
+def printGraph(name, before):
+	name = name + ".png"
+
+	G = nx.Graph()
+	G.add_edges_from(e)
+
+	node_list = []
+	labels = {}
+	for node in u:
+		labels[node[0]] = str(node[0])
+		if node[0] not in activate_set:
+			node_list.append(node[0])
+
+	nx.draw_networkx_nodes(G,pos,
+	                       nodelist=activate_set,
+	                       node_color="r",
+	                       node_size=10,
+	                   alpha=0.9)
+	nx.draw_networkx_nodes(G,pos,
+	                       nodelist=node_list,
+	                       node_color='black',
+	                       node_size=10,
+	                   alpha=0.9)
+	if before:
+		nx.draw_networkx_edges(G,pos,width=0.1,alpha=0.5,color='grey')
+	plt.savefig(name, format="PNG")
+	G.clear()
+	return
+
 u = [(1, {'pos': (rnd(),rnd())}), 
 	 (2, {'pos': (rnd(),rnd())}), 
 	 (3, {'pos': (rnd(),rnd())}), 
@@ -33,14 +62,10 @@ e = [(1,4, {'weight': 0.3}), (1,6, {'weight': 0.2}), (1,7, {'weight': 0.4}),
 	 (7,6, {'weight': 0.3}), (7,8, {'weight': 0.2}),
 	 (9,6, {'weight': 0.3}), (9,8, {'weight': 0.3})]
 
-sum_th = [0, 0, 0, 0, 0, 0, 0, 0, 0]
-activate_th = [0, 0.8, 0.6, 0.5, 0.6, 0, 0.5, 0.4, 0.4]
 activate_set = [1,6]
 activate_used = []
 
-G = nx.Graph()
-
-G.add_edges_from(e)
+printGraph("independent_before", True)
 
 for i in activate_set:
 	if i in activate_used:
@@ -55,33 +80,4 @@ for i in activate_set:
 	activate_used.append(i)
 
 
-node_list = []
-labels = {}
-for node in u:
-	labels[node[0]] = str(node[0])
-	if node[0] not in activate_set:
-		node_list.append(node[0])
-
-nx.draw_networkx_nodes(G,pos,
-                       nodelist=activate_set,
-                       node_color='r',
-                       node_size=350,
-                   alpha=0.8,
-                   with_labels=True)
-nx.draw_networkx_nodes(G,pos,
-                       nodelist=node_list,
-                       node_color='b',
-                       node_size=350,
-                   alpha=0.8,
-                   with_labels=True)
-nx.draw_networkx_labels(G,pos,labels,font_size=16)
-nx.draw_networkx_edges(G,pos,width=1.0,alpha=0.5)
-# G.add_nodes_from(u)
-# G.add_edges_from(e)
-# pos = nx.get_node_attributes(G,'pos')
-# print(pos)
-# nx.draw(G, pos, with_labels=True)
-labels = nx.get_edge_attributes(G,'weight')
-nx.draw_networkx_edge_labels(G,pos,edge_labels=labels)
-# plt.show()
-plt.savefig("independent.png", format="PNG")
+printGraph("independent_after", False)
